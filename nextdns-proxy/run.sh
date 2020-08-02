d6841e964971f953359488b4957b1f981536e62f
Justin Wood <jwood@me.com>
2020-08-02 16:12:07 -0700

    re-ordering this based on docs

--- nextdns-proxy/run.sh
+++ nextdns-proxy/run.sh
@@ -7,17 +7,17 @@ NEXTDNS_ARGUMENTS="-listen :8053 -report-client-info -log-queries"
 
 echo "Parsing configuration"
 
+while IFS='=' read -r -d '' n v; do
+    if [[ "$n" = "NEXTDNS_CONFIG_"* ]]; then
+        echo " => Found conditional config: $n => $v"
+        NEXTDNS_ARGUMENTS+=" -config $v"
+    fi
+done < <(env -0)
+
 if [ -n "$NEXTDNS_CONFIG" ]; then
     NEXTDNS_ARGUMENTS+=" -config $NEXTDNS_CONFIG_ID"
     echo " => Found base NextDNS Config: $NEXTDNS_CONFIG_ID"
 fi
-
-while IFS='=' read -r -d '' n v; do
-    if [[ "$n" = "NEXTDNS_CONFIG_"* ]]; then
-        echo " => Found additional config: $n => $v"
-        NEXTDNS_ARGUMENTS+=" -config $v"
-    fi
-done < <(env -0)
 
 if [ -n "$NEXTDNS_FORWARDING_DOMAIN" ]; then
     if [ -n "$NEXTDNS_FORWARDING_DNSIP" ]; then
